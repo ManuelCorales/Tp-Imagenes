daafaddd5e3e429c55690a59a353e48c0707f0fb
MFucci <mfucci@alumno.huergo.edu.ar>
2020-10-18 21:19:42 -0300

    Se agrega el informe finalizado - falta revisar

--- entregable/src/ejecutador.py
+++ entregable/src/ejecutador.py
@@ -1,6 +1,6 @@
 import os
 
 for n_threads in range(1,26):
-    for i in range(0,10): 
-        cmd = "./main edgeDetection " + str(n_threads) + " 10 30 ../imagenesIn/imagen_grande.ppm ../imagenesIn/imagen_grande.ppm"
+    for i in range(1,11): 
+        cmd = "./main edgeDetection " + str(n_threads) + " 1 1 imgs/imagen_grande.ppm imgs/imagen_grande.ppm"
         os.system(cmd)
